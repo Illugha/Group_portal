46ce05bdd0775be30f5db36c4ff2f32c70bdfd6f
Максим Мартинов <maxmartynov724@gmail.com>
2025-12-06 16:50:36 +0200

    Backend_forum

--- GroupPortal/urls.py
+++ GroupPortal/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Для login/logout
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('forum/', include('forum.urls'))  # Для login/logout
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
